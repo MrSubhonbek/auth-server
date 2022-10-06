e042bc773ec3a3c76dc7e959f62986be7cfe2efc
Subhonbek <basubhonbek@gmail.com>
2022-10-06 10:34:57 +0300

    init nest & add prisma

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 5000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(PORT);
+}
+bootstrap();
